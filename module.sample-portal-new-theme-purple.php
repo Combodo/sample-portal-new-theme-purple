f89eb56615909bdb07ee7fb1b3136142fc6a30b2
Taloc Erwan <33063836+etaloc@users.noreply.github.com>
2025-06-02 19:00:36 +0200

    Add files via upload

---
+++ module.sample-portal-new-theme-purple.php
@@ -0,0 +1,53 @@
+<?php
+//
+// iTop module definition file
+//
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'sample-portal-new-theme-purple/1.0.1',
+	array(
+		// Identification
+		//
+		'label' => 'Sample Portal (new theme) purple',
+		'category' => 'portal',
+
+		// Setup
+		//
+		'dependencies' => array(
+			'itop-portal-base/1.0.0',
+			'itop-portal-new-look-for-3.2-lts/0.1.0'	
+		),
+		'mandatory' => false,
+		'visible' => true,
+
+		// Components
+		//
+		'datamodel' => array(
+			//'model.sample-portal-new-theme.php'
+		),
+		'webservice' => array(
+			
+		),
+		'data.struct' => array(
+			// add your 'structure' definition XML files here,
+		),
+		'data.sample' => array(
+			// add your sample data XML files here,
+		),
+		
+		// Documentation
+		//
+		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+		'doc.more_information' => '', // hyperlink to more information, if any 
+
+		// Default settings
+		//
+		'settings' => array(
+			// Module specific settings go here, if any
+		),
+	)
+);
+
+
+?>
